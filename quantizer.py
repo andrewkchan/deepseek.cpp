91dc2db6c1819a59d556bfb039559962fb7e2aad
Andrew Chan <andrewkchan.akc@gmail.com>
2025-05-04 22:25:42 -0400

    add q3_k to convert.py

--- quantizer.py
+++ quantizer.py
@@ -1,9 +1,10 @@
 import torch
 import quantizer_cpp
+from typing import Literal
 
-def quantize_q2_k(tensor: torch.Tensor) -> torch.Tensor:
+def k_quantize(tensor: torch.Tensor, method: Literal["q2_k", "q3_k"]) -> torch.Tensor:
   """
-  Quantize a 2D float32 tensor to Q2_K format.
+  Quantize a 2D float32 tensor to Q2_K or Q3_K format.
   
   Args:
     tensor: Input tensor of shape (M, N) where N must be a multiple of 256
@@ -11,4 +12,9 @@ def quantize_q2_k(tensor: torch.Tensor) -> torch.Tensor:
   Returns:
     Quantized tensor of type uint8 and shape (M, sizeof(block_q2_K) * N/256) containing the block_q2_K data
   """ 
-  return quantizer_cpp.quantize_q2_k(tensor) 
+  if method == "q2_k":
+    return quantizer_cpp.quantize_q2_k(tensor) 
+  elif method == "q3_k":
+    return quantizer_cpp.quantize_q3_k(tensor) 
+  else:
+    raise ValueError(f"Invalid method: {method}")
